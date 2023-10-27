cd7eb17528f83cb37eaea465fadd79b082aa10b3
Marloneth <marloneth.9296@gmail.com>
2023-10-27 13:27:13 -0600

    fixing responsiveness

--- components/organisms/NewUserForm.tsx
+++ components/organisms/NewUserForm.tsx
@@ -29,47 +29,52 @@ export default function NewUserForm() {
   }
 
   return (
-    <form action={createNewUser} className="p-5 h-full flex flex-col">
+    <form
+      action={createNewUser}
+      className="p-5 h-[90%] md:h-fit w-[90%] lg:max-w-2xl mt-[10%] mx-auto flex flex-col border border-dark-gray rounded-lg"
+    >
       <legend className="text-2xl mb-8 text-center">
         Let us know what do you want to do in Chamburo
       </legend>
-      <div className="mb-8">
-        {rolesData.map(({ role, label }) => {
-          const lowerRole = getLowerRole(role)
+      <div className="flex flex-col md:flex-row">
+        <div className="mb-8 md:w-1/2">
+          {rolesData.map(({ role, label }) => {
+            const lowerRole = getLowerRole(role)
 
-          return (
-            <div className="mb-2" key={role}>
-              <input
-                required
-                type="radio"
-                id={lowerRole}
-                name="role"
-                value={role}
-                className="mr-2"
-                onChange={handleRadioSelection}
-              />
-              <label htmlFor={lowerRole} className="text-md">
-                {label}
-              </label>
-            </div>
-          )
-        })}
-      </div>
-
-      {userRole === Role.WORKER && (
-        <div className="">
-          <label htmlFor="occupation">What is your occupation?</label>
-          <input
-            type="text"
-            name="occupation"
-            required
-            className="border-2 rounded-md mt-2 p-2 border-accent-teal w-full"
-          />
+            return (
+              <div className="mb-2" key={role}>
+                <input
+                  required
+                  type="radio"
+                  id={lowerRole}
+                  name="role"
+                  value={role}
+                  className="mr-2"
+                  onChange={handleRadioSelection}
+                />
+                <label htmlFor={lowerRole} className="text-md">
+                  {label}
+                </label>
+              </div>
+            )
+          })}
         </div>
-      )}
+
+        {userRole === Role.WORKER && (
+          <div className="w-full md:w-1/2">
+            <label htmlFor="occupation">What is your occupation?</label>
+            <input
+              type="text"
+              name="occupation"
+              required
+              className="border-2 rounded-md mt-2 p-2 border-accent-teal w-full"
+            />
+          </div>
+        )}
+      </div>
       <button
         type="submit"
-        className="bg-vibrant-blue p-2 w-full rounded-md text-off-white mt-auto"
+        className="bg-vibrant-blue p-2 w-full mx-auto rounded-md text-off-white mt-auto md:mt-4 md:w-1/3"
       >
         Continue
       </button>
